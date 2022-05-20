172ca21e9e07c8ba775c7794a6df692068b7daf2
unknown <alexis2910e@gmail.com>
2022-05-19 23:28:06 -0500

    terminado

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -232,7 +232,10 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+  if (letra == "A" || letra == "E" || letra == "I" || letra == "O" || letra == "U" || letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u") {
+    return " Es vocal"
+}else{
+    return " Dato incorrecto"}
 }
 
 
